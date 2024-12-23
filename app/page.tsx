087887adfdd89cc4ca076f31cc315875c827dbea
erfan taheri <erfanta197@gmail.com>
2024-12-23 17:27:06 +0330

    Refactor Table components

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
-import Table from "@/components/Table";
+import DataTable from "@/components/DataTable";
 
 export default function Home() {
   return (
     <>
-      <Table>
-        <Table.TableHeader>
-          <Table.TableRow>
-            <Table.TableHead className="hidden w-[100px] sm:table-cell">
-              <span className="sr-only">Image</span>
-            </Table.TableHead>
-            <Table.TableHead>title</Table.TableHead>
-            <Table.TableHead>completed</Table.TableHead>
-            <Table.TableHead>
-              <span className="sr-only">Actions</span>
-            </Table.TableHead>
-          </Table.TableRow>
-        </Table.TableHeader>
-        <Table.TableBody>
-          {[].map((todo) => (
-            <span key={todo}>todo</span>
-          ))}
-        </Table.TableBody>
-      </Table>
+      <DataTable />
     </>
   );
 }
---
+++ components/DataTable.tsx
@@ -0,0 +1,29 @@
+import Table from "./ui/Table";
+
+const DataTable = () => {
+  return (
+    <>
+      <Table>
+        <Table.TableHeader>
+          <Table.TableRow>
+            <Table.TableHead className="hidden w-[100px] sm:table-cell">
+              <span className="sr-only">Image</span>
+            </Table.TableHead>
+            <Table.TableHead>title</Table.TableHead>
+            <Table.TableHead>completed</Table.TableHead>
+            <Table.TableHead>
+              <span className="sr-only">Actions</span>
+            </Table.TableHead>
+          </Table.TableRow>
+        </Table.TableHeader>
+        <Table.TableBody>
+          {[].map((todo) => (
+            <span key={todo}>todo</span>
+          ))}
+        </Table.TableBody>
+      </Table>
+    </>
+  );
+};
+
+export default DataTable;
